80c5dde7e710ac4d4635e99454706fb70e37d99a
geRivera <griveram@summa-sci.com>
2022-07-06 14:28:29 -0500

    cambios footer

--- summarket/app.js
+++ summarket/app.js
@@ -1,7 +1,7 @@
 // const { application, response } = require("express");
 const path = require('path');
 
-const express = require(`express`);
+const express = require("express");
 const app = express();
 
 app.use(express.static('public'));
@@ -12,7 +12,7 @@ app.listen(3030, () => {
 
 app.get('/', (request, response) => {
 <<<<<<< HEAD
- response.sendFile(path.resolve(__dirname, './vistas/home.html'));
+ response.sendFile(path.resolve(__dirname, "./vistas/home.html"));
 =======
   response.sendFile(path.resolve(__dirname, './vistas/home.html'));
 >>>>>>> d6c20ce8aeddffb9d2b250f9bf48267f8ec5aec9
