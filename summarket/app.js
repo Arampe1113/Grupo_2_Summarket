d6c20ce8aeddffb9d2b250f9bf48267f8ec5aec9
ccamargo8 <68198163+ccamargo8@users.noreply.github.com>
2022-07-06 10:28:06 -0500

    Modificación rutas de archivo app.js

--- summarket/app.js
+++ summarket/app.js
@@ -11,21 +11,21 @@ app.listen(3030, () => {
 });
 
 app.get('/', (request, response) => {
-  response.sendFile(path.resolve(__dirname, '/vistas/home.html'));
+  response.sendFile(path.resolve(__dirname, './vistas/home.html'));
 });
 
 app.get('/carritoDeCompras', (request, response) => {
-  response.sendFile(path.join(__dirname, '/vistas/carritodecompras.html'));
+  response.sendFile(path.resolve(__dirname, './vistas/carritodecompras.html'));
 });
 
 app.get('/register', (request, response) => {
-  response.sendFile(path.join(__dirname, '/vistas/register.html'));
+  response.sendFile(path.resolve(__dirname, './vistas/register.html'));
 });
 
 app.get('/login', (request, response) => {
-  response.sendFile(path.join(__dirname, '/vistas/login.html'));
+  response.sendFile(path.resolve(__dirname, './vistas/login.html'));
 });
 
 app.get('/detalleProducto', (request, response) => {
-  response.sendFile(path.join(__dirname, '/vistas/detalleProducto.html'));
+  response.sendFile(path.resolve(__dirname, './vistas/detalleProducto.html'));
 });
