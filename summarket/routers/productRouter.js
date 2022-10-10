30c56304af1dc5f4b4b6ad5dd001dc988b9274b4
ccamargo8 <68198163+ccamargo8@users.noreply.github.com>
2022-10-09 21:15:28 -0500

    Inclusión de sequelize y sql

--- summarket/routers/productRouter.js
+++ summarket/routers/productRouter.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
 const productCont = require('../controllers/productController');
 
 // Middlewares
