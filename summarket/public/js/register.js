57e8b9113df71f31f51bf57251d42521584df282
ccamargo8 <68198163+ccamargo8@users.noreply.github.com>
2022-10-27 19:00:35 -0500

    se agrega funcionalidad de validaciones en el front

--- summarket/public/js/register.js
+++ summarket/public/js/register.js
@@ -1,21 +1,29 @@
-// window.addEventListener('load', function () {
-//   let firstName = document.querySelector('#firstName');
-//   let lastName = document.querySelector('#lastName');
-//   let email = document.querySelector('#email');
-//   let password = document.querySelector('#password');
-//   let userImagee = document.querySelector('#userImage');
-//   let save = document.querySelector('.save-user');
-//   let p = document.querySelector('#mensaje-error');
-//   let bottonsave = document.querySelector('#botton-save-user');
+window.addEventListener('load', function () {
+  let firstName = document.querySelector('#firstName');
+  let lastName = document.querySelector('#lastName');
+  let errorName = document.querySelector('.errFirstName');
+  let email = document.querySelector('#email');
+  let password = document.querySelector('#password');
+  let userImagee = document.querySelector('#userImage');
+  let save = document.querySelector('.save-user');
+  let p = document.querySelector('#mensaje-error');
+  let bottonsave = document.querySelector('.save-user');
 
-//   bottonsave.addEventListener('click',function (e) {
-//     e.preventDefault();
-//     let errores = {}
-//     if(firstName.value.length < 2 || lastName.value.length<2) {
-//         errores.firstName="este campo debe tener al menos dos caracteres"
+  firstName.addEventListener('focusout', function (e) {
+    e.preventDefault();
+    let errores = {};
+    if (firstName.value.length < 2) {
+      errores.firstName = 'este campo debe tener al menos dos caracteres';
+    }
 
-//   }
+    if (Object.keys(errores).length >= 1) {
+      errorName.innerText = errores.firstName ? errores.firstName : '';
+    } else {
+      bottonsave.submit();
+    }
 
-//   )
-
-// });
+    //   if(firstName.value == null || firstName.value=0 || /^\s+$/.test(firstName)){
+    //     errores.firstName="Este campo no puede estar vacio"
+    //   }
+  });
+});
