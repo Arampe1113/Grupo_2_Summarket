c8aa1d16f9d92da86a95025c246bac753b7145ac
arampe1113 <aaramburop1113@gmail.com>
2022-10-31 19:35:43 -0500

    carrito de compras intetno

--- summarket/controllers/pedidodecarrito.js
+++ summarket/controllers/pedidodecarrito.js
@@ -1,4 +1,20 @@
-const carro= new carrito ();
-const carrito=document.getElementById("carrito");
-const productos =document.getElementById("lista-productos")
-const listaproductos= document.queryselector ("#")
+// const carro= new carrito ();
+// const carrito=document.getElementById("carrito");
+// const productos =document.getElementById("lista-productos")
+// const listaproductos= document.queryselector ("#")
+const db=require("../../database/models");
+const sequelize = db.sequelize;
+const productcontroller = requiere (productcontroller )
+
+document.addeventlistener ('DOMContentLoaded', () =>{
+    fetchData()
+})
+
+const fetchData=async()=>{
+    try{
+        const res= await fetch(productcontroller)
+        const data = await res.json()
+    } catch(error) {
+        console.log(error)
+    }
+}
