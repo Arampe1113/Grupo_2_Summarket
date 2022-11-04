b163aa48a7e39869e34faa65ea01a2f183938d57
arampe1113 <aaramburop1113@gmail.com>
2022-11-04 18:44:16 -0500

    carrito y pedido

--- summarket/controllers/pedidodecarrito.js
+++ summarket/controllers/pedidodecarrito.js
@@ -19,10 +19,61 @@
 //     }
 // }
 
-function addtocart(producto){
-console.log(producto)
-}
+// function addtocart(producto){
+// console.log(producto)
+// }
 
-module.exports = {
-    addtocart
-}
+// module.exports = {
+//     addtocart
+// }
+
+
+
+
+Window.onload = () => {
+    let name =document.querySelector("#name");
+    let price=document.querySelector("#price");
+    let description=document.querySelector("#description");
+    let discount=document.querySelector("#discount");
+  
+
+    const query=new URLSearchParams(location.search);
+  console.log (query.get("id"))
+
+  const idproducto=query.get("id");
+
+  if (idproducto) {
+    fetch("http://localhost:3030/api/v1/products" + idproducto)
+    .then ((response) => response.json())
+    .then ((data) => {
+        console.log(data);
+    })
+  }
+
+    ButtonAgregar.addEventListener("click", (e) =>{
+      e.preventDefalt();
+      const data = {
+      name: name.value,
+      price: price.value, 
+      discount: discount.value,
+  
+      };
+      let setting={
+        method:"POST",
+        body:json.stringify (data),
+        headers: {
+        "content-type": "application/json",
+        }
+      }
+  fetch("http://localhost:3030/api/v1/products".setting)
+  .then ((response) => response.json())
+  .then ((data)=>{
+    console.log(data);
+  });
+
+  console.log (data)
+    } )
+  
+  
+  }
+  
