02b41a6f8ee8fb04eac43509c70d3c8bf6ed8f76
ccamargo8 <68198163+ccamargo8@users.noreply.github.com>
2022-11-05 11:56:13 -0500

    Cambios en React

--- react/summarket-react/src/components/SideBar.js
+++ react/summarket-react/src/components/SideBar.js
@@ -8,75 +8,75 @@ import NotFound from './NotFound';
 
 function SideBar(){
     return(
-        <React.Fragment>
-            {/*<!-- Sidebar -->*/}
+    <React.Fragment>
+      {/*<!-- Sidebar -->*/}
             <ul className="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-                {/*<!-- Sidebar - Brand -->*/}
+        {/*<!-- Sidebar - Brand -->*/}
                 <a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
-                        <div className="sidebar-brand-icon">
+          <div className="sidebar-brand-icon">
                             <img className="w-100" src={image} alt="Summarket"/>
-                        </div>
-                </a>
+          </div>
+        </a>
 
-               {/*<!-- Divider -->*/}
+        {/*<!-- Divider -->*/}
                 <hr className="sidebar-divider my-0"/>
 
-                {/*<!-- Nav Item - Dashboard -->*/}
-                <li className="nav-item active">
-                    <Link className="nav-link" to="/">
-                        <i className="fas fa-fw fa-tachometer-alt"></i>
+        {/*<!-- Nav Item - Dashboard -->*/}
+        <li className="nav-item active">
+          <Link className="nav-link" to="/">
+            <i className="fas fa-fw fa-tachometer-alt"></i>
                         <span>Tablero - Summarket</span></Link>
-                </li>
+        </li>
 
-                {/*<!-- Divider -->*/}
+        {/*<!-- Divider -->*/}
                 <hr className="sidebar-divider"/>
 
-                {/*<!-- Heading -->*/}
-                <div className="sidebar-heading">Acciones</div>
+        {/*<!-- Heading -->*/}
+        <div className="sidebar-heading">Acciones</div>
 
-                {/*<!-- Nav Item - Users -->*/}
-                <li className="nav-item">
-                <Link className="nav-link" to="/UsersInDb">
-                        <i className="fas fa-fw fa-folder"></i>
-                        <span>Usuarios en BD</span>
-                    </Link>
-                </li>
+        {/*<!-- Nav Item - Users -->*/}
+        <li className="nav-item">
+          <Link className="nav-link" to="/UsersInDb">
+            <i className="fas fa-fw fa-folder"></i>
+            <span>Usuarios en BD</span>
+          </Link>
+        </li>
 
-                {/*<!-- Nav Item - Products -->*/}
-                <li className="nav-item">
-                <Link className="nav-link" to="/ProductsInDb">
-                        <i className="fas fa-fw fa-folder"></i>
-                        <span>Productos en BD</span>
-                    </Link>
-                </li>
-     
-             {/*<!-- Nav Item - Tables -->*/}
-             <li className="nav-item nav-link">
-                <Link className="nav-link" to="/ContentRowProducts">
-                        <i className="fas fa-fw fa-table"></i>
+        {/*<!-- Nav Item - Products -->*/}
+        <li className="nav-item">
+          <Link className="nav-link" to="/ProductsInDb">
+            <i className="fas fa-fw fa-folder"></i>
+            <span>Productos en BD</span>
+          </Link>
+        </li>
+
+        {/*<!-- Nav Item - Tables -->*/}
+        <li className="nav-item nav-link">
+          <Link className="nav-link" to="/ContentRowProducts">
+            <i className="fas fa-fw fa-table"></i>
                         <span>Tablas Usuarios y Productos</span></Link>
-                </li>
+        </li>
 
-                {/*<!-- Divider -->*/}
+        {/*<!-- Divider -->*/}
                 <hr className="sidebar-divider d-none d-md-block"/>
-            </ul>
-            {/*<!-- End of Sidebar -->*/}
-   
-            <Switch>
-                <Route exact path="/">
-                    <SideBar />
-                </Route>
-                <Route path="/UsersInDb">
-                    <UsersInDb />
-                </Route>
-                <Route path="/ProductsInDb">
-                    <ProductsInDb />
-                </Route>               
-                <Route component={NotFound} />
-            </Switch>
+      </ul>
+      {/*<!-- End of Sidebar -->*/}
 
-        </React.Fragment>
+      <Switch>
+        <Route exact path="/">
+          <SideBar />
+        </Route>
+        <Route path="/UsersInDb">
+          <UsersInDb />
+        </Route>
+        <Route path="/ProductsInDb">
+          <ProductsInDb />
+        </Route>
+                <Route component={NotFound} />
+      </Switch>
+
+    </React.Fragment>
     )
 }
 export default SideBar;
