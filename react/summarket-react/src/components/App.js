abe0a0f3f2e4117a1cca0bfd6e9afedc2ebb4ad0
Geovani Rivera Mesa <griveram@summa-sci.com>
2022-11-06 18:53:42 -0500

    cambios en app.js react

--- react/summarket-react/src/components/App.js
+++ react/summarket-react/src/components/App.js
@@ -2,27 +2,26 @@ import React from 'react';
 import SideBar from './SideBar';
 import {useState, useEffect, useRef} from 'react';
 import '../css/App.css';
-import Product from './ProductsInDb';
 
 function App() {
 
-  // const urlProducts = 'http://localhost:3030/api/v1/products'
-  // const urlUsers = 'http://localhost:3030/api/v1/users'
+  const urlProducts = 'http://localhost:3030/api/v1/products'
+  const urlUsers = 'http://localhost:3030/api/v1/users'
 
 // //llamar a una API de listados
 // const url = 'https://jsonplaceholder.typicode.com/todos/'
 // //hook para ver estados
-//   const [user, setUser] = useState()
-//   const fetchApi = async () => {
-//     const response = await fetch(urlUsers)
-//     const responseJSON = await response.json()
-//     setUser(responseJSON)
-//   }
+  const [url, setUser] = useState()
+  const fetchApi = async () => {
+    const response = await fetch(urlUsers)
+    const responseJSON = await response.json()
+    setUser(responseJSON)
+  }
 
 //   //hook que permite tomar del ciclo de vida y ejecutarlo por una vez al iniciar la app
-//   useEffect(() => {
-//     fetchApi()},
-//   [])
+  useEffect(() => {
+    fetchApi()},
+  [])
   
 return (
   <React.Fragment>
