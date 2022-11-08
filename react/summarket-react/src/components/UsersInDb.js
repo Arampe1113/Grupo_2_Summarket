738288c9fd8c55bea0b9d0d6452b4ea1ab5213da
Geovani Rivera Mesa <griveram@summa-sci.com>
2022-11-07 19:58:10 -0500

    vista usuarios en react

--- react/summarket-react/src/components/UsersInDb.js
+++ react/summarket-react/src/components/UsersInDb.js
@@ -1,52 +1,54 @@
 import React, { useEffect, useState } from "react";
-import image from '../images/summa2.png';
 
 
-// const User = () => {
-//     const [users, setUsers] = useState([])
-//     const fetchUserData = () => {
-//       fetch('http://localhost:3030/api/v1/users')
-//         .then(response => {
-//           return response.json()
-//         })
-//         .then(data => {
-//           setUsers(data)
-//         })
-//     }
-//       useEffect(() => {
-//         fetchUserData()
-//       }, [])
-//   return (
-//       <div>
-//       <img src={image} alt="Summarket"/>
-//         {users.length > 0 && (
-//           <ul>
-//             {users.map(user => (
-//               <li key={user.id}>{user.name}</li>
-//             ))}
-//           </ul>
-//         )}
-//       </div>
-//   );
-// }
+const User = () => {
+    const [users, setUsers] = useState([])
 
-// export default User();
+    const fetchUserData = () => {
+      fetch('http://localhost:3030/api/v1/users')
+        .then(response => {
+          return response.json()
+        })
+        .then(response => {
+          console.log(response.data)
+          setUsers(response.data)
+          })
+    }
 
+      useEffect(() => {
+        fetchUserData()
+      }, [])
 
-function UsersInDb(){
   return (
-      <main className="usuarios"> 
-          <img src={image} alt="Summarket"/>
-          <h2 id="usuarios">Usuarios</h2>
-          <article>
-             <h2>Lorem, ipsum.</h2>
-             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni mollitia blanditiis natus dicta error, cum alias ipsam libero odio impedit, fuga at dolore voluptatem illum pariatur cupiditate fugit, ullam amet?</p>
-              <button>Ver más</button>
-          </article>
-      </main>
+      <div>
+        {users.length > 0 && (
+          <ul>
+            {users.map(user => (
+              <li key={user.id}>{user.firstName} E-mail: {user.email} </li>
+            ))}
+          </ul>
+        )}
+      </div>
+  );
+}
 
-  )
-}  
+export default User;
 
-  export default UsersInDb;
+
+// function UsersInDb(){
+//   return (
+//       <main className="usuarios"> 
+//           <img src={image} alt="Summarket"/>
+//           <h2 id="usuarios">Usuarios</h2>
+//           <article>
+//              <h2>Lorem, ipsum.</h2>
+//              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni mollitia blanditiis natus dicta error, cum alias ipsam libero odio impedit, fuga at dolore voluptatem illum pariatur cupiditate fugit, ullam amet?</p>
+//               <button>Ver más</button>
+//           </article>
+//       </main>
+
+//   )
+// }  
+
+//   export default UsersInDb;
   
